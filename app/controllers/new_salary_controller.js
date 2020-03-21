918ecf9f646ee310b3ca60afa7075778e94d34c8
sonia jasuja <sjasuja2@sapient.com>
2020-03-21 19:44:09 +0000

    added logic for new salary calculation api

--- app/controllers/new_salary_controller.js
+++ app/controllers/new_salary_controller.js
@@ -1,6 +1,8 @@
 // const newSalary = require('../models/new_salary_model.js');
 const supertest = require('supertest');
 const {employeesEndpoint} = require('../../config/api.config');
+const {TITLE_CONSTANTS} = require('../constants/employees_constants');
+
 
 // Find the Salary of an employee
 exports.Calculate = (req, res) => {
@@ -12,14 +14,35 @@ exports.Calculate = (req, res) => {
         });
     };
 
+    if (req.header('performanceRating')<0 || req.header('performanceRating')>5){
+        return res.status(400).send({
+            message: "performanceRating should be between 0 to 5"
+        });
+    }
+
     supertest(employeesEndpoint)
         .get(req.query.employeeName)
         .expect(200)
         .end((err, employeeResponse) => {
             if (err) throw err;
-            const newSalaryResponse = employeeResponse.body.currentSalary * req.header('performanceRating');
+            let appraisal = req.header('performanceRating') * getSalaryMultiplier (employeeResponse);
+            const newSalaryResponse = employeeResponse.body.currentSalary + appraisal
             res.send({
                 "newSalary": newSalaryResponse
             })
         })
+
+        function getSalaryMultiplier (employeeResponse) {
+        
+            let salaryMultiplier = 0
+            if (employeeResponse.body.title === TITLE_CONSTANTS.ENGINEER ) {
+                salaryMultiplier=2000;
+            } else if (employeeResponse.body.title === TITLE_CONSTANTS.MANAGER){
+                salaryMultiplier=3000;
+            } else if (employeeResponse.body.title === TITLE_CONSTANTS.DIRECTOR) {
+                salaryMultiplier=4000
+            }
+            return salaryMultiplier
+     
+        }
 };
